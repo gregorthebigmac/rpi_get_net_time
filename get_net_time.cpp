4f91830eb81487b3a1df5522877e4e69695639ae
gregorthebigmac <gregorthebigmac@gmail.com>
2019-05-03 12:28:22 -0500

    figured out the seg fault. It's working now.

--- get_net_time.cpp
+++ get_net_time.cpp
@@ -10,12 +10,9 @@ command cmd;
 int main() {
 	vector<string> terminal_feedback, error_list;
 	cmd.exec("curl google.com", terminal_feedback, error_list);
-	if (terminal_feedback[0][0] == '<') {
-		cout << "We got a network connection!" << endl;
-		cout << "Dumping contents of [terminal_feedback]..." << endl;
-		for (int i = 0; i < terminal_feedback.size(); i++) {
-			cout << terminal_feedback[i];
-		}
+	if (terminal_feedback.size() > 0) {
+		if (terminal_feedback[0][0] == '<') {
+			cout << "We got a network connection!" << endl; }
 	}
 	else {
 		cout << "No network connection! Dumping contents of [error_list], followed by [terminal_feedback]..." << endl;
