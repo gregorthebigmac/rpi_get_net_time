54904ba3b4cc421ca6033fefbfb4f66efb1c7c6f
gregorthebigmac <gregorthebigmac@gmail.com>
2019-05-03 11:59:29 -0500

    Seems to be working. More testing!

--- get_net_time.cpp
+++ get_net_time.cpp
@@ -11,7 +11,11 @@ int main() {
 	vector<string> terminal_feedback, error_list;
 	cmd.exec("curl google.com", terminal_feedback, error_list);
 	if (terminal_feedback[0][0] == '<') {
-		cout << "We got a network connection";
+		cout << "We got a network connection!" << endl;
+		cout << "Dumping contents of [terminal_feedback]..." << endl;
+		for (int i = 0; i < terminal_feedback.size(); i++) {
+			cout << terminal_feedback[i];
+		}
 	}
 	else {
 		cout << "No network connection! Dumping contents of [terminal_feedback]..." << endl;
