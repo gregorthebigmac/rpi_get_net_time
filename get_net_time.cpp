9fad14a522d3d7c6da8c9a2fb9bd9e512e72d2a1
gregorthebigmac <gregorthebigmac@gmail.com>
2019-05-03 12:35:16 -0500

    I think it's ready for prime time! Just need a bit more testing.

--- get_net_time.cpp
+++ get_net_time.cpp
@@ -1,3 +1,4 @@
+#include <unistd.h>
 #include "command.hpp"
 
 using std::cout;
@@ -9,27 +10,18 @@ command cmd;
 
 int main() {
 	vector<string> terminal_feedback, error_list;
-	cmd.exec("curl google.com", terminal_feedback, error_list);
-	if (terminal_feedback.size() > 0) {
-		if (terminal_feedback[0][0] == '<') {
-			cout << "We got a network connection!" << endl; }
-	}
-	else {
-		cout << "No network connection! Dumping contents of [error_list], followed by [terminal_feedback]..." << endl;
-		if (error_list.size() > 0) {
-			for (int i = 0; i < error_list.size(); i++) {
-				cout << error_list[i];
-			}
-		}
-		else cout << endl << endl << "Nothing in [error_list]!" << endl << endl;
+	bool got_network_connection = false;
+
+	while (!got_network_connection) {
+		cmd.exec("curl -s google.com", terminal_feedback, error_list);
 		if (terminal_feedback.size() > 0) {
-			for (int i = 0; i < terminal_feedback.size(); i++) {
-				cout << terminal_feedback[i];
-			}
+			if (terminal_feedback[0][0] == '<') {
+				got_network_connection = true; }
+			else usleep(1000 * 1000);
 		}
-		else cout << endl << endl << "Nothing in [terminal_feedback]!" << endl << endl;
-		return 1;
+		else usleep(1000 * 1000);
 	}
+	
 	string get_time = "sudo date -s \"$(wget -qSO- --max-redirect=0 google.com 2>&1 | grep Date: | cut -d' ' -f5-8)Z\"";
 	cmd.exec(get_time.c_str(), terminal_feedback, error_list);
 	return 0;
